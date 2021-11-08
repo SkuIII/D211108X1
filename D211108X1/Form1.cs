2ec7751e53b90f07e41acb5309527981ac2819b8
ogus02 <ogus0247@gmail.com>
2021-11-08 12:14:32 +0100

    initial commit

---
+++ D211108X1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D211108X1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var MyPeople = new ContactEntities().People;
+
+            
+            foreach (Person item in MyPeople)
+            {
+                listBox1.Items.Add(item.ToString());
+            }
+        }
+    }
+
+    public partial class Person
+    {
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
